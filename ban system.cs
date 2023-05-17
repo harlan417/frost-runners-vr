311c4a4eac5bef50df562dab8d09ad6a572126c7
carvedpoemytvr <85445553+harlan417@users.noreply.github.com>
2023-05-17 09:04:38 -0500

    Update ban system.cs

--- ban system.cs
+++ ban system.cs
@@ -21,5 +21,35 @@ void Update()
 
 
 {
+
 // If the current number of reports reaches the amount required for a ban, a ban should activate.
 
+
+using UnityEngine;
+
+
+using GoogleDocsAPI;
+
+
+using Google.Apis.Auth.OAuth2;
+
+
+using Google.Apis.Sheets.v1;
+
+
+using UnityEngine.UI;
+
+
+using System.Collections;
+
+
+[System.Serializable]
+
+
+public class BanReportAndBan : MonoBehaviour {
+
+
+public List<PlayerReportBean> ReportsToSend = new List<PlayerReportBean>();
+
+
+`public int num
